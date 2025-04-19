819f5a60d15523147026b0e4ffafcd2664dab6a1
Ouail <ouail.impactiq@gmail.com>
2025-04-19 14:26:30 +0200

    Update

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'be.impactiq.app',
+  appName: 'IMPACT IQ',
+  webDir: 'dist'
+};
+
+export default config;
